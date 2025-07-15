43aa5cf8fd639e2b41baeb41ea756801a656bcd4
mario443-has <123mariochitan@gmail.com>
2025-07-15 11:20:38 -0500

    ya

--- lumiere_glamour/settings.py
+++ lumiere_glamour/settings.py
@@ -67,7 +67,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'store.context_processors.get_common_context',
             ],
         },
     },

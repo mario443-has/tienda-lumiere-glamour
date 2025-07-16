06c781a4e7a1dd7b728b6529b2c9414736d880ec
mario443-has <123mariochitan@gmail.com>
2025-07-16 14:25:37 -0500

    errores productos

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,7 @@ from .views import CategoriaListView # Agrega esta importación
 
 urlpatterns = [
     path('', views.inicio, name='home'),
-    path('producto/<int:product_id>/', views.producto_detalle, name='producto_detalle'), # CAMBIO: 'producto_id' cambiado a 'product_id'
+    path('producto/<int:pk>/', views.producto_detalle, name='producto_detalle'), # CAMBIO: 'producto_id' cambiado a 'product_id'
     path("google1e60e56990e838db.html", views.google_verification, name="google_verification"),
     path('agregar-al-carrito/', views.agregar_al_carrito, name='agregar_al_carrito'), # Nueva URL para añadir al carrito
     path('api/buscar-productos/', views.api_buscar_productos, name='api_buscar_productos'), # Endpoint para búsqueda en vivo

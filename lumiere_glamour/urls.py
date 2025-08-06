e3bc9b32b1880b73bc7eae9e0679a0d4bbafa052
mario443-has <123mariochitan@gmail.com>
2025-08-06 17:41:53 -0500

    Fallback media serving in production

--- lumiere_glamour/urls.py
+++ lumiere_glamour/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,3 +13,16 @@ urlpatterns = [
 # Para servir archivos multimedia (como imágenes) en modo desarrollo
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if (
+    not settings.DEBUG
+    and settings.DEFAULT_FILE_STORAGE
+    == "django.core.files.storage.FileSystemStorage"
+):
+    urlpatterns += [
+        path(
+            f"{settings.MEDIA_URL.lstrip('/')}" + "<path:path>",
+            serve,
+            {"document_root": settings.MEDIA_ROOT},
+        )
+    ]

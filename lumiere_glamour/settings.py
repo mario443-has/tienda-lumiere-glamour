e84be54fff1ba42eb85512f56881cd280c68eafb
mario443-has <123mariochitan@gmail.com>
2025-08-19 21:08:49 -0500

    arreglo settings

--- lumiere_glamour/settings.py
+++ lumiere_glamour/settings.py
@@ -85,7 +85,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.humanize",
-    "django.contrib.sitemaps" 
+    "django.contrib.sitemaps", 
     "store", 
     "ckeditor", 
     "ckeditor_uploader", 
